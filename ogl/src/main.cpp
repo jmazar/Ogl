0ff0ba09138b7ca0fcdefca3753fd4375f55e608
unknown <Jeffrey@.(none)>
2012-05-26 19:24:07 -0700

    Made a triangle.

--- ogl/src/main.cpp
+++ ogl/src/main.cpp
@@ -107,6 +107,7 @@ int WINAPI WinMain(
 	EnableOpenGL( hWnd, &hDC, &hRC);
 
 	bool quit = false;
+	float theta = 0.0f;
 	
 	while( !quit ) {
 		if( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) ) {
@@ -119,9 +120,24 @@ int WINAPI WinMain(
 			}
 		}
 		else {
-			glClearColor( 0.0f, 1.0f, 0.0f, 0.0f);
+			glClearColor( 0.0f, 0.0f, 0.0f, 0.0f);
 			glClear( GL_COLOR_BUFFER_BIT );
+
+			glPushMatrix();
+			glRotatef( theta, 0.0f, 0.0f, 1.0f );
+			glBegin( GL_TRIANGLES );
+			glColor3f( 1.0f, 0.0f, 0.0f ); 
+			glVertex2f( 0.0f, 1.0f );
+			glColor3f( 0.0f, 1.0f, 0.0f ); 
+			glVertex2f( 0.87f, -0.5f );
+			glColor3f( 0.0f, 0.0f, 1.0f ); 
+			glVertex2f( -0.87f, -0.5f );
+			glEnd();
+			glPopMatrix();
+
 			SwapBuffers( hDC );
+
+			//theta += 1.0f;
 		}
 	}
 
