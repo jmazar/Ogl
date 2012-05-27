4c743679d7692fad32c7925cb17f1666b3fcd9bf
unknown <Jeffrey@.(none)>
2012-05-26 18:43:39 -0700

    Edited project and actually added window creation.

--- ogl/src/main.cpp
+++ ogl/src/main.cpp
@@ -3,7 +3,76 @@
 #include <gl/GLU.h>
 #include <stdio.h>
 
-int main () {
-	printf("Hello world!\n");
-	return 0;
+char const g_szClassName[] = "ogl";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+
+int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow) {
+	WNDCLASSEX windowsClass;
+	HWND hWnd;
+	MSG msg;
+
+	//Registering the windows class
+	windowsClass.cbSize        = sizeof(WNDCLASSEX);
+	windowsClass.style         = 0;
+	windowsClass.lpfnWndProc   = WndProc;
+	windowsClass.cbClsExtra    = 0;
+	windowsClass.cbWndExtra    = 0;
+	windowsClass.hInstance     = hInstance;
+	windowsClass.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+	windowsClass.hCursor       = LoadCursor(NULL, IDC_ARROW);
+	windowsClass.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	windowsClass.lpszMenuName  = NULL;
+	windowsClass.lpszClassName = g_szClassName;
+	windowsClass.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+
+	if(!RegisterClassEx(&windowsClass)) {
+		MessageBox(NULL, "Window registration failed.", "Window Error",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	hWnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		g_szClassName,
+		"ogl",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+		NULL, NULL, hInstance, NULL);
+
+	if(NULL == hWnd) {
+		MessageBox(NULL, "Window creation failed!", "Window Error",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while(GetMessage( &msg, NULL, 0, 0) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
 }
