9744d92958eedca187b36609d2fc9ca7364f2943
Jeffrey Mazar <jeffrey.mazar@gmail.com>
2012-06-17 15:24:27 -0700

    Trying to add textures.
    Added texture coord calculation and then modified the fragment shader.
    Just kind of black now though.

--- ogl/src/shaders/fragment.glsl
+++ ogl/src/shaders/fragment.glsl
@@ -10,25 +10,24 @@ uniform vec3 vEyeLocation;
 
 smooth in vec3 vNormalW;
 smooth in vec3 vPosW;
-in vec2 TexCoord;
+smooth in vec2 vTexCoord;
 
 uniform sampler2D Texture;
 
 void main(void) {
 	vec3 worldNormal = normalize(vNormalW);
 	vec3 vLightDir = normalize(vLightPosition - vPosW);
-
-	float s = max(dot(vLightDir, worldNormal), 0.0f);
-
 	vec3 vToEye = normalize(vEyeLocation - vPosW);
-	vec3 vReflect = reflect(vLightDir, worldNormal);
+	vec3 vReflect = normalize(reflect(vLightDir, worldNormal));
 
-	float t = pow(max(dot(vReflect, vToEye), 0.0), 128.0);
+	float s = max(0.0, dot(vLightDir, worldNormal));
+
+	float t = pow(max(0.0, dot(vToEye, vReflect)), 128.0);
 
 	vec4 specular = t * vec4(0.65, 0.65, 0.25, 1);
 	vec4 diffuse = s * diffuseColor;
 
-	vec3 texColor = vec3(texture(Texture, TexCoord));
+	diffuse = diffuse * texture(Texture, vTexCoord);
 
-	gl_FragColor = vec4(texColor, 1.0) + specular + diffuse + vec4(0.0, 0.0, 0.02, 1.0);
+	gl_FragColor = specular + diffuse + vec4(0.0, 0.0, 0.02, 1.0);
 }
--- ogl/src/shaders/vertex.glsl
+++ ogl/src/shaders/vertex.glsl
@@ -5,27 +5,19 @@ uniform mat4 mvpMatrix;
 
 in vec4 in_vPosition;
 in vec3 in_vNormal;
+in vec2 in_vTexCoord;
 
-out vec2 TexCoord;
 smooth out vec3 vNormalW;
 smooth out vec3 vPosW;
-
-vec2 SphereMap(const in vec3 position, const in vec3 normal) {
-	float m;
-	vec3 r, u;
-	u = normalize(position);
-	r = reflect(u, normal);
-	r.z += 1.0;
-	m = 0.5 * inversesqrt( dot(r,r));
-	return r.xy * m + 0.5;
-}
+smooth out vec2 vTexCoord;
 
 void main(void) {
-	vNormalW = mul(modelMatrix, vec4(in_vNormal, 1.0)).xyz;
+	vNormalW = (modelMatrix * vec4(in_vNormal, 1.0)).xyz;
 	vNormalW = normalize(vNormalW);
 
-	vPosW = mul(modelMatrix, in_vPosition).xyz;
-	TexCoord = SphereMap(vPosW, vNormalW);
+	vPosW = (modelMatrix * in_vPosition).xyz;
+
+	vTexCoord = in_vTexCoord;
 
 	gl_Position = mvpMatrix * in_vPosition; 
 }
