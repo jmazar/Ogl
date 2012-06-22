5e799278137438e85972d4e29cf7e9d528a80480
Jeffrey Mazar <jeffrey.mazar@gmail.com>
2012-06-22 14:52:01 -0700

    Updated camera to be able to get eye position.

--- ogl/src/camera.cpp
+++ ogl/src/camera.cpp
@@ -1,21 +1,28 @@
 #include "camera.h"
 
 Camera::Camera() {
+	m_CameraTranslation = glm::mat4(1.0);
+	m_CameraRotation = glm::mat4(1.0);
 }
 
 glm::mat4 const Camera::GetViewMatrix() const {
-  return glm::inverse(m_CameraRotation * m_CameraTranslation);
+	return glm::inverse(m_CameraRotation * m_CameraTranslation);
 }
 
 glm::mat4 const Camera::GetWorldMatrix() const {
-  return m_CameraRotation * m_CameraTranslation;
+	return m_CameraRotation * m_CameraTranslation;
 }
 
 void Camera::SetCameraTranslation(glm::vec3 const & in_translate) {
-  m_CameraTranslation = glm::translate(glm::mat4(1.0), in_translate);
+	m_CameraTranslation = glm::translate(glm::mat4(1.0), in_translate);
 }
 
 void Camera::SetCameraRotation(float in_angle, glm::vec3 const & in_axis) {
-  m_CameraRotation = glm::rotate(glm::mat4(1.0), in_angle, in_axis);
+	m_CameraRotation = glm::rotate(glm::mat4(1.0), in_angle, in_axis);
+}
+
+glm::vec4 const Camera::GetEyePosVec() const {
+	glm::vec4 row = glm::column(m_CameraRotation * m_CameraTranslation,3);
+	return row;
 }
 
--- ogl/src/camera.h
+++ ogl/src/camera.h
@@ -2,17 +2,19 @@
 #define _CAMREA
 
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/matrix_access.hpp"
 
 class Camera {
 public:
-  Camera();
-  glm::mat4 const GetViewMatrix() const;
-  glm::mat4 const GetWorldMatrix() const;
-  void SetCameraTranslation(glm::vec3 const & in_translate);
-  void SetCameraRotation(float in_angle, glm::vec3 const & in_axis);
+	Camera();
+	glm::mat4 const GetViewMatrix() const;
+	glm::mat4 const GetWorldMatrix() const;
+	void SetCameraTranslation(glm::vec3 const & in_translate);
+	void SetCameraRotation(float in_angle, glm::vec3 const & in_axis);
+	glm::vec4 const GetEyePosVec() const;
 private:
-  glm::mat4 m_CameraTranslation;
-  glm::mat4 m_CameraRotation;
+	glm::mat4 m_CameraTranslation;
+	glm::mat4 m_CameraRotation;
 };
 
 #endif 
